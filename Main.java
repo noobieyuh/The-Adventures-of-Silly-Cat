653a1cca9585c3d4e2020e38f2b974482a6828a4
noobie <150881131+noobieoncrack@users.noreply.github.com>
2024-11-11 21:34:58 -0600

    added lots of dialogue and user choice

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class Main {
         TimeUnit.SECONDS.sleep(1);
         System.out.print(".");
         TimeUnit.SECONDS.sleep(1);
-        System.out.println(".");
+        System.out.println(".\n");
         TimeUnit.SECONDS.sleep(1);
 
         // Title screen
@@ -31,8 +31,12 @@ public class Main {
         System.out.println("\nMade with love by noobieyuh <3\n");
 
         System.out.println("Press enter to continue...");
-        System.in.read();
+        input.nextLine();
 
+        // start of chapter 1
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Chapter 1: Awakening\n");
+        TimeUnit.SECONDS.sleep(3);
         System.out.println("...");
         TimeUnit.SECONDS.sleep(1);
         System.out.println("...");
@@ -45,13 +49,111 @@ public class Main {
         TimeUnit.SECONDS.sleep(2);
         System.out.println("You see a black cat holding their paw out.");
         TimeUnit.SECONDS.sleep(2);
-        System.out.println("???: Finally, it took me 30 minutes to wake you up!");
+        System.out.println("???: Finally, it took me " + (int)((Math.random() * 60) + 2) + " minutes to wake you up!");
         TimeUnit.SECONDS.sleep(2);
         System.out.println("???: So, what's your name?");
 
         // User input for name
         String mainName = input.nextLine();
-        
+        System.out.println("???: " + mainName + "... huh. I never heard that name before.");
+        TimeUnit.SECONDS.sleep(2);
+        // introducing character that acts as a guide
+        System.out.println("???: Well, since you told me your name, I'll tell you mine.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: My name is Moon. It's nice to meet you I guess.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: Well, you're probably wondering why you're in this cabin.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("\nYou do wonder.\n");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println(mainName + ": Yeah, why am I in this cabin..?");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: Well... you sorta just, appeared.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("\n(Huh..?)\n");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("(I just... appeared?)\n");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: Judging by your face, you must be confused as well...");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println(mainName + ": Yeah... I am...");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: " + mainName + ", try to think back, what is your purpose here?");
+        TimeUnit.SECONDS.sleep(2);
+        // role assignment
+        String userRole = "";
+        System.out.println("(...My purpose here? I was a...)\n");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("1. Wizard - A powerful being able to conjure spells.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("2. Warrior - Swords and shields, you can move and fight.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("3. Tank - Big and bad. You can take a hit.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("4. Thief - Thiefs take goodies you can use anytime.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("5. Average Joe - Civilian level...\n");
+        TimeUnit.SECONDS.sleep(2);
+        // selecting roles
+        int roleSelect = input.nextInt();
+        if(roleSelect == 1)
+        {
+            userRole = "Wizard";
+        }
+        if(roleSelect == 2)
+        {
+            userRole = "Warrior";
+        }
+        if(roleSelect == 3)
+        {
+            userRole = "Tank";
+        }
+        if(roleSelect == 4)
+        {
+            userRole = "Thief";
+        }
+        if(roleSelect == 5)
+        {
+            userRole = "Average Joe";
+        }
+        if(roleSelect < 1 || roleSelect > 5)
+        {
+            System.out.println("Please choose a number between 1 - 5 next time. The default role is Average Joe.");
+            userRole = "Average Joe";
+        }
+        // end of role select
+        // making a MainCharacter class
+        MainCharacter protagonist = new MainCharacter(mainName, userRole);
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println(mainName + ": I remember... I'm a " + userRole + "! I think...");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: Wow! Good to know you haven't lost that many memories...\n");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("\nYou hear a bell ring in the distance...");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: I guess it's that time already, huh?");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println(mainName + ": Huh? What does that mean Moon?");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: I'll tell you later, just follow me " + mainName + "!");
+        TimeUnit.SECONDS.sleep(2);
+        // end of chapter 1
+        // start of chapter 2
 
+        System.out.println("Chapter 2: Chaos\n");
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("Moon: Wow... This is awful!");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: There's so many dogs... everywhere!");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("\n(...What..?)\n");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println(mainName + ": What's wrong with dogs?");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Moon: If I'm being honest... everything.");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println(mainName + ": What did dogs do to you?");
+        TimeUnit.SECONDS.sleep(2);
+        
     }
 }
